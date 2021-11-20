a4174237d0c165234ae19cca6a18be8d6a73acf3
pedrommf7 <pedro2mferreira@gmail.com>
2021-11-20 18:21:51 +0000

    criar entidades exceto meia encoemnda e entrega

--- main.pl
+++ main.pl
@@ -3,10 +3,10 @@
 
 
 
-assert -> adiciona predicados
+% assert -> adiciona predicados
 
 
-
+%escrever factos de estafetas, clientes e encomendas para termos já alguns na BD
 
 % Factos:
 % ----------- veiculo(tipo, velocidadeMedia, cargaMax)
@@ -14,10 +14,65 @@ veiculo(bicicleta, 10, 5).
 veiculo(mota, 35, 20).
 veiculo(carro, 25, 100). 
 
+cliente(mauricio, 0).
+cliente(sebastiao, 3).
 
-% ----------- entrega(id, peso, volume, freguesia, morada, prazo, dataInicio, dataFim, cliente, estafeta, avaliacao)
+estafeta(merdas, 0, 0).
+estafeta(burro, 5, 4.3).
 
 
+
+
+
+% ----------- encomenda(id, idCliente, peso, volume, freguesia, morada, prazo)
+% ----------- estafeta(id, nrEntregas, avaliacao)
+% ----------- cliente(id, nrEncomendas)
+% ----------- entrega(idEncomenda, idEstafeta veiculo, DataInicio, DataFim, avaliacao)
+
+ajuda(geral, "escrever todos os comandos de ajuda aqui").
+% mudar esta parte
+ajuda(encomenda, "Usar a funcao createEncomenda() com os parâmetros: 
+                Id, IdCliente, IdEstafeta, Peso, Volume, Freguesia, 
+                Morada, Prazo, DataInicio, DataFim, Avaliacao").
+
+help(X) :- ajuda(X, Y), write(Y). % tentar tirar o true que aparece depois
+
+
+
+
+createEstafeta(Id) :- \+estafeta(Id, _, _), dynamic(estafeta/3), assert(estafeta(Id, 0, 0)) write("Estafeta adicionado"), !;
+                        write("Estafeta já existente")
+
+
+createCliente(Id) :- \+cliente(Id, _), dynamic(cliente/2), assert(cliente(Id, 0)), write("Cliente adicionado"), !;
+                        write("Cliente já existente").
+
+
+
+                %fazer os teste de id da encomenda e assim antes, esxiste id cliente, estafeta e assim
+createEncomenda(Id, IdCliente, IdEstafeta, Peso, Volume, Freguesia, 
+                Morada, Prazo, DataInicio, DataFim, Avaliacao) :- 
+                    encomenda(Id, _, _, _, _, _, _, _, _, _ ), write("Encomenda já existente"), !;
+                    \+encomenda(_, IdCliente, _, _, _, _, _, _, _, _ ), write("Cliente não existente"), !;
+                    \+encomenda(_, _, IdEstafeta, _, _, _, _, _, _, _ ), write("Estafeta não existente"), !;
+
+        assert(encomenda(Id, IdCliente, IdEstafeta, Peso, Volume, Freguesia, 
+                            Morada, Prazo, DataInicio, DataFim, Avaliacao)).
+
+
+
+
+
+
+
+
+
+
+
+replace_existing_fact(OldFact, NewFact) :-
+    call(OldFact),
+    retract(OldFact),
+    assertz(NewFact).
 
 
 % evitar duplicação de entregas?? ter lista de entregas com id
